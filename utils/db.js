a6aefa0870a050ff340b1d0ad591f570ace58d55
gumball09 <vivianpham0904@gmail.com>
2021-09-04 01:42:17 -0400

    Utilities, Helper functions

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,5 @@
 const { Sequelize } = require('sequelize');
 const config = require('../utils/config')
-const logger = require('../utils/logger')
 // Set up Postgresql connection to AWS
 const sequelize = new Sequelize(config.DB_NAME, config.DB_USERNAME, config.DB_PWD, {
     host: config.DB_HOST,
